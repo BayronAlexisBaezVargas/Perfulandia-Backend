3937ffb40736a7a131a15509f988b2a6418810ae
vrayiRAX <ia.badilla@duocuc.cl>
2025-05-25 20:22:58 -0400

    commit pedido

--- perfumeria-pedido/src/main/java/com/perfumeria/perfumeria_pedido/model/pedido.java
+++ perfumeria-pedido/src/main/java/com/perfumeria/perfumeria_pedido/model/pedido.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Builder
 
 public class pedido {
-    //atributos
+    //atributos.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPedido;
