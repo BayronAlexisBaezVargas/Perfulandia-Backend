795f99f4b27b5391c4d7ef327fb5f379867dbb82
natalia000000 <nataliaparedesbustamante@gmail.com>
2025-05-26 13:21:04 -0400

    hola

--- src/main/java/com/Inventario/Microservicio_Inventario/controller/InventarioController.java
+++ src/main/java/com/Inventario/Microservicio_Inventario/controller/InventarioController.java
@@ -7,8 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-//a
 
+//hola
 @RestController
 @RequestMapping("/api/inventario")
 
