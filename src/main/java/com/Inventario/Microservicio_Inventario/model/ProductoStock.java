30e3b95500f81a6821da2061908d7782ec2923ae
BayronAlexisBaezVargas <bay.baez@duocuc.cl>
2025-05-26 23:26:23 -0400

    Commit final del proyecto funcional y parte de natalia que no su pudo subir por errores de github conexiones funcionales y apis funcionales

--- src/main/java/com/Inventario/Microservicio_Inventario/model/ProductoStock.java
+++ src/main/java/com/Inventario/Microservicio_Inventario/model/ProductoStock.java
@@ -2,8 +2,11 @@ package com.Inventario.Microservicio_Inventario.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.*;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class ProductoStock {
 
@@ -12,23 +15,5 @@ public class ProductoStock {
     private int stock;
     private String nombre;
 
-
-    public ProductoStock() {}
-
-    public ProductoStock(Long id,String nombre, int stock){
-        this.id = id;
-        this.nombre=nombre;
-        this.stock=stock;
-    }
-
-    //Getter y setter
-    public Long getId(){return id; }
-    public void  setId (Long id) {this.id = id; }
-
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-
-    public int getStock() { return stock; }
-    public void setStock(int stock) { this.stock = stock; }
 
 }
